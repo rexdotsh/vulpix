e58ba597f970c4022223b90d806df3460dd8c0bd
mridul <65942753+rexdotsh@users.noreply.github.com>
2025-06-07 16:09:59 +0530

    fix: add vercel blob url to remotePatterns next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd4izytthgfmbpa4h.public.blob.vercel-storage.com', // vercel blob url
+      },
+    ],
+  },
+};
 
 export default nextConfig;
