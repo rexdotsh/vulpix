602e271f5e28797d47860eb1a15428c61001b182
mridul <65942753+rexdotsh@users.noreply.github.com>
2025-06-15 19:11:25 +0530

    fix: next config url

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'd4izytthgfmbpa4h.public.blob.vercel-storage.com', // vercel blob url
       },
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud', // ipfs gateway url
+      },
     ],
   },
 };
